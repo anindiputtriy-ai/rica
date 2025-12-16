c6627ac5d0a8446f8aac5e59abdb80c6978adc2c
anindiputtriy-ai <anindiputtriy@gmail.com>
2025-12-16 18:55:46 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,6 +42,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="facebook-domain-verification" content="uel8f1t73450rreuz9d7tvlzznohtf" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
       >
